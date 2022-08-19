8ed811a130f45c60c6be070e6bf23e488b9792c5
Nhat <dsiver144@gmail.com>
2022-08-19 10:53:12 +0700

    ADD: AutotileUtils.calcIndexBy8Direction

--- js/plugins/DSI_Sys1_AutotileCore.js
+++ js/plugins/DSI_Sys1_AutotileCore.js
@@ -5,14 +5,15 @@
 //========================================================================
 /*:
 * @author dsiver144
-* @plugindesc (v1.0) An utility plugin for handling autotiling for Nhat Nguyen's plugins.
+* @plugindesc (v1.1) An utility plugin for handling autotiling for Nhat Nguyen's plugins.
 * @help 
 * Handle autotiling calculating.
 * 
 */
-const rpgMakerTileIdSpecial = "0 = 46, 2 = 44, 8 = 45, 10 = 39, 11 = 38, 16 = 43, 18 = 41, 22 = 40, 24 = 33, 26 = 31, 27 = 30, 30 = 29, 31 = 28, 64 = 42, 66 = 32, 72 = 37, 74 = 27, 75 = 25, 80 = 35, 82 = 19, 86 = 18, 88 = 23, 90 = 15, 91 = 14, 94 = 13, 95 = 12, 104 = 36, 106 = 26, 107 = 24, 120 = 21, 122 = 7, 123 = 6, 126 = 5, 127 = 4, 208 = 34, 210 = 17, 214 = 16, 216 = 22, 218 = 11, 219 = 10, 222 = 9, 223 = 8, 248 = 20, 250 = 3, 251 = 2, 254 = 1, 255 = 0"
+const RpgMakerTileIdSpecial = "0 = 46, 2 = 44, 8 = 45, 10 = 39, 11 = 38, 16 = 43, 18 = 41, 22 = 40, 24 = 33, 26 = 31, 27 = 30, 30 = 29, 31 = 28, 64 = 42, 66 = 32, 72 = 37, 74 = 27, 75 = 25, 80 = 35, 82 = 19, 86 = 18, 88 = 23, 90 = 15, 91 = 14, 94 = 13, 95 = 12, 104 = 36, 106 = 26, 107 = 24, 120 = 21, 122 = 7, 123 = 6, 126 = 5, 127 = 4, 208 = 34, 210 = 17, 214 = 16, 216 = 22, 218 = 11, 219 = 10, 222 = 9, 223 = 8, 248 = 20, 250 = 3, 251 = 2, 254 = 1, 255 = 0"
+const EightDirections = [[-1, 1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]];
 var BitmaskToTileIDTable = {}
-rpgMakerTileIdSpecial.split(",").forEach((str) => {
+RpgMakerTileIdSpecial.split(",").forEach((str) => {
     const [mask, tileId] = str.split("=").map(n => Number(n));
     BitmaskToTileIDTable[mask] = tileId;
 });
@@ -97,6 +98,19 @@ AutotileUtils.calcAutoTileIndex = function (bitmask) {
     const bottomRight = (bottom && right && bitmask & (2 ** 7)) > 0
     const refinedBitmask = topLeft * (2 ** 0) + top * (2 ** 1) + topRight * (2 ** 2) + left * (2 ** 3) + right * (2 ** 4) + bottomLeft * (2 ** 5) + bottom * (2 ** 6) + bottomRight * (2 ** 7);
     return BitmaskToTileIDTable[refinedBitmask];
+}
+/**
+ * Calculate autotile index using 8 direction check
+ * @param {(pos: Vector2) => boolean} callback 
+ * @returns 
+ */
+AutotileUtils.calcIndexBy8Direction = function(callback) {
+    const bitmask = 0;
+    EightDirections.forEach((offset, index) => {
+        const value = callback(new Vector2(...offset)) ? 1 : 0;
+        bitmask += value * (2 ** index);
+    })
+    return BitmaskToTileIDTable[bitmask];
 }
 /**
  * Copy Correct Part Of Autotile Bitmap To Destinate Bitmap By `autoTileId`
