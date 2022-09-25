555ae0812102821e2036bf2df382cdba9de4e655
Nhat Nguyen <nhat.nguyen-long@ubisoft.com>
2022-09-25 22:54:09 +0700

    Add a script to playtest using npm

--- js/plugins/DSI_Sys1_Steam.js
+++ js/plugins/DSI_Sys1_Steam.js
@@ -22,6 +22,37 @@ class SteamAPI {
         } catch(err) {
             console.warn("Error occured when initializing greenwork.", err);
         }
+        if (this.isInitialized) {
+            const steamIdObj = this.getSteamID();
+            this.username = steamIdObj.screenName;
+            this.steamId = steamIdObj.steamId;
+            this.gameName = this.currentGameName();
+            console.log(`%cSteam API initialized successfully!\nName: ${this.username} (${this.steamId})\nGame: ${this.gameName}`, "color:green; font-size:30px");
+        } else {
+            console.warn(`Steam API initialized failed!`, "color:red");
+        }
+    }
+    /**
+     * Get Steam ID
+     * @returns {any}
+     */
+    getSteamID() {
+        return SteamAPI.inst.greenworks.getSteamId();
+    }
+    /**
+     * Get current app install dir
+     * @returns {string}
+     */
+    getCurrentGameInstallDir() {
+        return SteamAPI.inst.greenworks.getCurrentGameInstallDir();
+    }
+    /**
+     * Get Current Game Name
+     * @returns {string}
+     */
+    currentGameName() {
+        const name = this.getCurrentGameInstallDir();
+        return name.substring(name.lastIndexOf("\\") + 1, name.length - 1);
     }
     /**
      * Activate Achievement
