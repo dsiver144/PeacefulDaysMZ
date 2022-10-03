f5b69c90d6bd0ac1c6cead5ae2aa9cf0c8e718ae
Nhat <dsiver144@gmail.com>
2022-10-03 17:40:13 +0700

    Update setting menu

--- js/plugins/DSI_Sys4_MainMenu_Settings.js
+++ js/plugins/DSI_Sys4_MainMenu_Settings.js
@@ -1,3 +1,10 @@
+/** @enum */
+const OptionControl = {
+    "OK": 'ok',
+    "Left": 'left',
+    "Right": 'right',
+}
+
 class Window_Settings extends Window_Command {
     /**
      * This class handle bag menu display for Peaceful Days
@@ -10,8 +17,35 @@ class Window_Settings extends Window_Command {
      * Make Command List
      */
     makeCommandList() {
-        for (var i = 0; i < 10; i++) {
-            this.addCommand('Command #' + i, 'item', true);
-        }
+        // for (var i = 0; i < 10; i++) {
+        //     this.addCommand('Command #' + i, 'item', true);
+        // }
+        this.addOption('Lb_Option_BGMVolume', {type: "slider", min: 0, max: 100, step: 1, fastStep: 10}, this.onBGMVolumeControl.bind(this));
+    }
+    /**
+     * 
+     * @param {OptionControl} type 
+     */
+    onBGMVolumeControl(type) {
+        console.log(type);
+    }
+    /**
+     * 
+     * @param {*} textKey 
+     * @param {*} type 
+     * @param {*} method 
+     */
+    addOption(textKey, data, method) {
+        this.addCommand(textKey, textKey, true, data);
+    }
+    /**
+     * @inheritdoc
+     */
+    drawItem(index) {
+
+    }
+
+    cursorLeft() {
+        console.log("Left");
     }
 }
