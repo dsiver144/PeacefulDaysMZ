0ae8f10a74ebf7dce47b2b93715bf1339ca0aa9a
Nhat <dsiver144@gmail.com>
2022-10-10 11:21:44 +0700

    [Controller System] Press mouse will switch to keyboard mode

--- js/plugins/DSI_Sys1_ControllerSystem.js
+++ js/plugins/DSI_Sys1_ControllerSystem.js
@@ -432,6 +432,7 @@ Scene_Map.prototype.isMenuCalled = function () {
 var DSI_Sys1_ControllerSystem_TouchInput__onMouseDown = TouchInput._onMouseDown;
 TouchInput._onMouseDown = function (event) {
     Input.setState(256 + event.button, true);
+    Input.setInputMode('keyboard');
     DSI_Sys1_ControllerSystem_TouchInput__onMouseDown.call(this, event);
 };
 
