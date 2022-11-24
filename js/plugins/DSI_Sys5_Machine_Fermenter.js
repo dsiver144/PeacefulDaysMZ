8fa80b8df78485000982f178ef4ce2896f809ca0
Nhat Nguyen Long <nhat.nguyen-long@ubisoft.com>
2022-11-24 15:25:43 +0700

    Update fermenter

--- js/plugins/DSI_Sys5_Machine_Fermenter.js
+++ js/plugins/DSI_Sys5_Machine_Fermenter.js
@@ -12,4 +12,41 @@ class Fermenter extends FarmMachine {
     spriteClass() {
         return Sprite_Fermenter;
     }
+    /**
+     * @inheritdoc
+     */
+    bottomSize() {
+        return { x: 2, y: 2 };
+    }
+    /**
+     * @inheritdoc
+     */
+    imageRect() {
+        return {
+            x: 0,
+            y: 0,
+            width: 68,
+            height: 108
+        }
+    }
+    /**
+     * @inheritdoc
+     */
+    interactTextKey() {
+        return 'Lb_Open';
+    }
+    /**
+     * @inheritdoc
+     */
+    displayAnchor() {
+        return { x: 0.0, y: 0.65 };
+    }
+    /**
+     * Get interaction range
+     * @returns {Rectangle}
+     */
+    interactionRange() {
+        const { x, y } = this.position;
+        return { x: x, y: y, width: 2, height: 2 };
+    }
 }
